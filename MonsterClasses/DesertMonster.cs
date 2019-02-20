a41f5da758db03a9d1387fc3e43f84c4e0ffa41b
Jared Heimforth <heimfo1@ad.nmc.edu>
2019-02-20 17:03:24 -0500

    Add project files.

---
+++ MonsterClasses/DesertMonster.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonsterClasses
+{
+    public class DesertMonster : Monster
+    {
+        private string _homeDesert;
+        private int _limbs;
+
+
+        public int Limbs
+        {
+            get { return _limbs; }
+            set { _limbs = value; }
+        }
+
+
+        public string HomeDesert
+        {
+            get { return _homeDesert; }
+            set { _homeDesert = value; }
+        }
+
+
+        public override bool IsHappy()
+        {
+            return true;
+        }
+
+    }
+}
---
+++ MonsterClasses/SeaMonster.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonsterClasses
+{
+    public class SeaMonster : Monster
+    {
+
+        private string _homeSea;
+        private bool _hasGills;
+
+        public bool HasGills
+        {
+            get { return _hasGills; }
+            set { _hasGills = value; }
+        }
+
+        public string HomeSea
+        {
+            get { return _homeSea; }
+            set { _homeSea = value; }
+        }
+
+        public override bool IsHappy()
+        {
+            //if (_hasGills)
+            //{
+            //    return true;
+            //}
+            //else
+            //{
+            //    return false;
+            //}
+
+            return _hasGills ? true : false;
+        }
+    }
+}
